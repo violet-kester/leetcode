355cc2ac079c122135f7880f613d7a9e1aa14d7f
Violet Kester <60329197+violet-kester@users.noreply.github.com>
2023-07-01 16:09:02 -0400

    Time: 61 ms (54.47%), Space: 41.9 MB (68.09%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  let sIdx = 0;
+  let tIdx = 0;
+
+  while (sIdx < s.length && tIdx < t.length) {
+    if (!(t.includes(s[sIdx]))) {
+      return false;
+    }
+
+    if (s[sIdx] === t[tIdx]) {
+      sIdx++;
+    }
+    tIdx++;
+  }
+
+  return sIdx === s.length;
+};
