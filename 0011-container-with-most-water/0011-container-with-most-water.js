55e98e13ee9924eca13a3a1461434629883eef71
Violet Kester <60329197+violet-kester@users.noreply.github.com>
2023-07-11 20:12:00 -0400

    Time: 77 ms (61.35%), Space: 49.7 MB (65.56%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -8,17 +8,10 @@ var maxArea = function (height) {
     let right = height.length - 1;
 
     while (left < right) {
-        let area = calculateArea([left, height[left]], [right, height[right]]);
-        maxArea = area > maxArea ? area : maxArea;
+        let area = (Math.min(height[left], height[right]) * (right - left))
+        maxArea = Math.max(area, maxArea);
         height[left] < height[right] ? left++ : right--;
     }
 
     return maxArea;
 };
-
-var calculateArea = function (line1, line2) {
-    let length = Math.abs(line1[0] - line2[0]);
-    let width = Math.min(line1[1], line2[1]);
-
-    return length * width;
-}
