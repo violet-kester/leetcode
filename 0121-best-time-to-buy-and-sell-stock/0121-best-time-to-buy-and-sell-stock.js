9ff8ff7ed7c1ce6575573c7e8b587903fee8a4cc
Violet Kester <60329197+violet-kester@users.noreply.github.com>
2023-08-27 23:06:05 -0400

    Time: 76 ms (60.67%), Space: 51 MB (96.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,29 +2,20 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-    // left and right pointers iterate over days `i`
-    let buyDay = 0; // left pointer
-    let sellDay = 1; // right pointer
-    let profit = 0;
-    let currentMaxProfit = 0;
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let buy = 0;
+    let sell = 1;
 
-    while (sellDay < prices.length) {
-        profit = prices[sellDay] - prices[buyDay];
-        // if profit is negative,
-        // update left and right pointers
-        if (profit <= 0) {
-            // set buy day to sell day - the current minimum value
-            buyDay = sellDay;
-            // increment sell day
-            sellDay++;
+    while (sell < prices.length) {
+        if (prices[buy] > prices[sell]) {
+            buy = sell;
+            sell = buy + 1;
         } else {
-            // calculate and set current max profit
-            currentMaxProfit = profit > currentMaxProfit ? profit : currentMaxProfit;
-            // increment sell day
-            sellDay++;
+            profit = prices[sell] - prices[buy];
+            maxProfit = profit > maxProfit ? profit : maxProfit;
+            sell++;
         }
-    }
-
-    return currentMaxProfit;
+    }   
+    return maxProfit;
 };
